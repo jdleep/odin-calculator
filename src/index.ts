ce70b2163fdd5aea5c41e7b0d4e9849d373e13b7
Jeff Leep <3838327+jdleep@users.noreply.github.com>
2024-04-14 13:31:20 -0400

    add typescript calculator class

---
+++ src/index.ts
@@ -0,0 +1,24 @@
+interface BinaryOps {
+    [i: string]: (a: number, b: number) => number;
+}
+
+// class CalcMethods implements BinaryOps {
+//     '+' = (a: number, b: number) => a + b;
+//     '-' = (a: number, b: number) => a - b;
+//     '*' = (a: number, b: number) => a * b;
+// }
+
+class Calculator {
+    methods: BinaryOps = {
+        '+': (a: number, b: number) => a + b,
+        '-': (a: number, b: number) => a - b,
+        '*': (a: number, b: number) => a * b,
+    }
+
+    operate(a: number, b: string, c: number) {
+        return this.methods[b](a, c);
+    }
+}
+
+
+export { Calculator }
