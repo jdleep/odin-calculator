ca610b6dc5cbf7ce0c7b3ab3f4913d963738899a
Jeff Leep <3838327+jdleep@users.noreply.github.com>
2024-04-21 20:43:27 -0400

    Add tests and digit entry for operand1

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,18 @@ interface BinaryOps {
     [i: string]: (a: number, b: number) => number;
 }
 
+interface UnaryOps {
+    [i: string]: (a: number) => number;
+}
+
+type calcState = {
+    stage: 'OPERAND1' | 'OPERAND2';
+    operand1: string;
+    operator: string;
+    operand2: string;
+    displayValue: string;
+}
+
 // class CalcMethods implements BinaryOps {
 //     '+' = (a: number, b: number) => a + b;
 //     '-' = (a: number, b: number) => a - b;
@@ -9,19 +21,71 @@ interface BinaryOps {
 // }
 
 class Calculator {
-    methods: BinaryOps = {
+
+    state: calcState = {
+        stage: 'OPERAND1',
+        operand1: '',
+        operator: '',
+        operand2: '',
+        displayValue: ''
+    }
+
+    digits = new Set(['1','2', '3', '4', '5', '6', '7', '8', '9', '0']) 
+
+    binaryOps: BinaryOps = {
         '+': (a: number, b: number) => a + b,
         '-': (a: number, b: number) => a - b,
         '*': (a: number, b: number) => a * b,
         '/': (a: number, b: number) => a / b
-    }
+    };
 
-    operate(a: number, b: string, c: number) {
-        if (this.methods[b]) {
-            return this.methods[b](a, c);
+    unaryOps: UnaryOps = {
+        '+/-': (a: number) => a === 0 ? 0 : -a,
+        '%': (a:number) => a / 100,
+        '=': (a:number) => a
+    };
+
+    binaryOperate(a: number, b: string, c: number) {
+        if (this.binaryOps[b]) {
+            return this.binaryOps[b](a, c);
         } else {
             throw new Error("Operator not found in calculator");
         }
+    };
+
+    unaryOperate(a: number, b: string) {
+        if (this.unaryOps[b]) {
+            return this.unaryOps[b](a);
+        } else {
+            throw new Error("Operator not found in calculator");
+        }
+    };
+
+    private enterDigit(digit: string, stage: string) {
+        let state = this.state;
+        let operand = state.operand1;
+        if (operand === '0' && digit === '0') {
+            return state;
+        }
+        operand = operand + digit;
+        state.displayValue = operand;
+    }
+
+    calculate(entry: string): calcState {
+        let state = this.state;
+        if (state.stage === 'OPERAND1') {
+            if (entry.match(/^[0-9]+$/) != null) {
+                this.enterDigit(entry, state.stage);
+            } else {
+                state.operator = entry;
+                state.stage = 'OPERAND2';
+            }
+        } else {
+            if (entry.match(/^[0-9]+$/) != null) {
+                this.enterDigit(entry, state.stage);
+            } 
+        }
+        return state;
     }
 }
 
--- test/index.test.ts
+++ test/index.test.ts
@@ -3,58 +3,158 @@ import { describe, test, expect } from "@jest/globals"
 
 let calc = new Calculator();
 //todo test overflows
-describe('Calculator operate addition', () => {
+describe('Calculator binary operators', () => {
     test('Addition with positive numbers', () => {
-        expect(calc.operate(1,'+',2)).toEqual(3);
+        expect(calc.binaryOperate(1,'+',2)).toEqual(3);
     });
     test('Addition with zero', () => {
-        expect(calc.operate(0,'+',2)).toEqual(2);
+        expect(calc.binaryOperate(0,'+',2)).toEqual(2);
     });
     test('Addition with negative number', () => {
-        expect(calc.operate(5,'+',-100)).toEqual(-95);
+        expect(calc.binaryOperate(5,'+',-100)).toEqual(-95);
     });
-});
-
-describe('Calculator operate subtraction', () => {
     test('Subtraction with positive numbers', () => {
-        expect(calc.operate(50, '-', 25)).toEqual(25);
+        expect(calc.binaryOperate(50, '-', 25)).toEqual(25);
     });
     test('Subtraction with negative numbers', () => {
-        expect(calc.operate(-5,'-',-1395)).toEqual(1390);
+        expect(calc.binaryOperate(-5,'-',-1395)).toEqual(1390);
     });
     test('Subtraction with zero', () => {
-        expect(calc.operate(0,'-',0)).toEqual(0);
+        expect(calc.binaryOperate(0,'-',0)).toEqual(0);
+    });
+    test('Multiplication with positive numbers', () => {
+        expect(calc.binaryOperate(5,'*',12)).toEqual(60);
+    }); 
+    test('Multiplication with negative numbers', () => {
+        expect(calc.binaryOperate(-1,'*',-2)).toEqual(2);
+    });
+    test('Multiplication with zero', () => {
+        expect(calc.binaryOperate(0,'*',1)).toEqual(0);
+    });
+    test('Division with positive numbers', () => {
+        expect(calc.binaryOperate(15,'/',3)).toEqual(5);
+    });
+    test('Divide positive by negative number', () => {
+        expect(calc.binaryOperate(300,'/',60)).toEqual(5);
+    });
+    test('Divide by zero', () => {
+        expect(calc.binaryOperate(1,'/',0)).toEqual(Infinity);
     });
 });
 
-describe('Calculator operate multiplication', () => {
-    test('Multiplication with positive numbers', () => {
-        expect(calc.operate(5,'*',12)).toEqual(60);
-    }); 
-    test('Multiplication with negative numbers', () => {
-        expect(calc.operate(-1,'*',-2)).toEqual(2);
+describe('Calculator unary operators', () => {
+    test('Sign with positive number', () => {
+        expect(calc.unaryOperate(1,'+/-')).toEqual(-1);
     });
-    test('Multiplication with zero', () => {
-        expect(calc.operate(0,'*',1)).toEqual(0);
+    test('Sign with zero', () => {
+        expect(calc.unaryOperate(0,'+/-')).toEqual(0);
+    });
+    test('Sign with negative number', () => {
+        expect(calc.unaryOperate(-120340, '+/-')).toEqual(120340);
+    });
+    test('Percent with negative number', () => {
+        expect(calc.unaryOperate(-120340, '%')).toEqual(-1203.40);
+    });
+    test('Percent with positive number', () => {
+        expect(calc.unaryOperate(120340, '%')).toEqual(1203.40);
+    });
+    test('Percent with zero', () => {
+        expect(calc.unaryOperate(0, '%')).toEqual(0);
     });
 })
 
-describe('Calculator operate division', () => {
-    test('Division with positive numbers', () => {
-        expect(calc.operate(15,'/',3)).toEqual(5);
-    });
-    test('Divide positive by negative number', () => {
-        expect(calc.operate(300,'/',60)).toEqual(5);
-    });
-    test('Divide by zero', () => {
-        expect(calc.operate(1,'/',0)).toEqual(Infinity);
+describe('Calculator errors', () => {
+    test('Use an operator that isn\'t defined', () => {
+        expect(() => {
+            calc.binaryOperate(300,'?',30);
+        }).toThrow('Operator not found in calculator');
     });
 });
 
-describe('Calculator operator not present', () => {
-    test('Use an operator that isn\'t defined', () => {
-        expect(() => {
-            calc.operate(300,'?',30);
-        }).toThrow('Operator not found in calculator');
+
+describe('Calculator digit entry', () => {
+    test('Operand1 digit entry', () => {
+        let calcDigit = new Calculator();
+        calcDigit.calculate('1');
+        calcDigit.calculate('2');
+        calcDigit.calculate('3');
+        expect(calcDigit.calculate('4')).toEqual({
+            stage: 'OPERAND1',
+            operand1: '1234',
+            operator: '',
+            operand2: '',
+            displayValue: '1234'
+        });
+    });
+
+    test('Operand1 digit entry2', () => {
+        let calcDigit = new Calculator();
+        calcDigit.calculate('4');
+        calcDigit.calculate('8');
+        expect(calcDigit.calculate('0')).toEqual({
+            stage: 'OPERAND1',
+            operand1: '480',
+            operator: '',
+            operand2: '',
+            displayValue: '480'
+        });
+    });
+
+    test('Operand1 enter zero multiple times', () => {
+        let calcDigit = new Calculator();
+        calcDigit.calculate('0');
+        calcDigit.calculate('0');
+        expect(calcDigit.calculate('0')).toEqual({
+            stage: 'OPERAND1',
+            operand1: '0',
+            operator: '',
+            operand2: '',
+            displayValue: '0'
+        });
+    });
+
+    test('Operand2 digit entry', () => {
+        let calcDigit = new Calculator();
+        calcDigit.calculate('4');
+        calcDigit.calculate('8');
+        calcDigit.calculate('+');
+        calcDigit.calculate('6');
+        expect(calcDigit.calculate('0')).toEqual({
+            stage: 'OPERAND2',
+            operand1: '48',
+            operator: '+',
+            operand2: '60',
+            displayValue: '60'
+        });
+    });
+
+    test('Operand2 digit entry', () => {
+        let calcDigit = new Calculator();
+        calcDigit.calculate('7');
+        calcDigit.calculate('4');
+        calcDigit.calculate('+');
+        calcDigit.calculate('2');
+        expect(calcDigit.calculate('9')).toEqual({
+            stage: 'OPERAND2',
+            operand1: '74',
+            operator: '+',
+            operand2: '29',
+            displayValue: '29'
+        });
+    });
+
+    test('Operand2 enter zero multiple times', () => {
+        let calcDigit = new Calculator();
+        calcDigit.calculate('7');
+        calcDigit.calculate('4');
+        calcDigit.calculate('+');
+        calcDigit.calculate('0');
+        expect(calcDigit.calculate('0')).toEqual({
+            stage: 'OPERAND2',
+            operand1: '74',
+            operator: '+',
+            operand2: '0',
+            displayValue: '0'
+        });
     });
 });
